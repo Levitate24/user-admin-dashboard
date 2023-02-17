01946166eb64c7b63b786efccd1c88e91591043a
Purvish17 <123947080+Purvish17@users.noreply.github.com>
2023-02-17 18:17:16 +0530

    update in About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,4 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import "./styles.css"
-import { BrowserRouter } from "react-router-dom"
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-)
+export default function About() {
+    return <h1>About us <br/> contact us:</h1>
+  }
+  
